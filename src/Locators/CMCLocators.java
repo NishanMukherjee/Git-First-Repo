d25433b6a521ff75bc68d168be7270c9eeefd9ba
Nishan Mukherjee <nishanmukherjee210@gmail.com>
2024-07-01 06:56:34 -0700

    Commit to master

--- src/Locators/CMCLocators.java
+++ src/Locators/CMCLocators.java
@@ -3,7 +3,7 @@ package Locators;
 public class CMCLocators {
 	public static void main(String[] args)
 	{
-	System.out.println("This is from Git Hub user");
+	System.out.println("This is from Git Hub user. Code updated on 01/07/2024");
 	}
 
 }
